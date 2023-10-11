d1a00cbe6fc7d0ec31b2bd457b0901c6a0b87523
C <CM.vanLenthe@student.han.nl>
2023-10-11 15:37:44 +0200

    Init

---
+++ Data/AppDatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class AppDatabaseContext : DbContext
+    {
+        public AppDatabaseContext(DbContextOptions<AppDatabaseContext> opt) : base(opt)
+        {
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
---
+++ Data/PrepDatabase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDatabase
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDatabaseContext>());
+            }
+        }
+
+        private static void SeedData(AppDatabaseContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("Seeding Data into Database...");
+
+                context.Platforms.AddRange(
+                    new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "5000.00" },
+                    new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "500.00" }
+                );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Data already exists!");
+            }
+        }
+    }
+}
